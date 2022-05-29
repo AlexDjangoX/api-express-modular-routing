f88a1bf3b75aa8671240b3fda9d7e008d88c09f4
AlexDjangoX <alexmonk17@gmail.com>
2022-05-29 16:01:26 +0200

    refactor

--- src/routers/films.js
+++ src/routers/films.js
@@ -2,47 +2,59 @@ const express = require("express");
 const router = express.Router();
 let { films } = require("../../data.js");
 
-router.get("/", function (req, res) {
-  let filteredFilms;
-  if (Object.entries(req.query).length !== 0) {
-    filteredFilms = films.filter(
-      (film) => film.director === req.query.director
-    );
+router.get("/", (req, res) => {
+  let filteredFilms = [];
+  const queryArray = Object.entries(req.query);
+  console.log("8...", req.query);
+  console.log("9...", queryArray);
+  // [
+  //   [ 'director', 'Quentin Tarantino' ],
+  //   [ 'title', 'Django Unchained' ]
+  // ]
+  // ? query ?
+  if (queryArray.length !== 0) {
+    queryArray.forEach((item) => {
+      // director / title
+      filteredFilms = films.filter((el) => el[item[0]] === req.query[item[0]]);
+    });
+
     return res.json({ filteredFilms });
   }
   res.json({ films });
 });
 
 function check(res, req) {
-  if (!films.some((film) => film.id === +req.params.id)) {
+  const idExists = films.some((film) => film.id === +req.params.id);
+  if (!idExists) {
     return res.status(400).json({
       error: `film id: ${req.params.id} does not exist`,
     });
   }
 }
 
-router.get("/:id", function (req, res) {
+router.get("/:id", (req, res) => {
   check(res, req);
+  console.log(req.query);
   const film = films.find((film) => film.id === +req.params.id);
 
   res.json({ film });
 });
 
-router.post("/", function (req, res) {
+router.post("/", (req, res) => {
   let id = films.length + 1;
   const film = { ...req.body, id };
   films.push(film);
   res.json({ film });
 });
 
-router.delete("/:id", function (req, res) {
+router.delete("/:id", (req, res) => {
   check(res, req);
   const film = films.find((film) => film.id === +req.params.id);
   films = films.filter((film) => film.id !== +req.params.id);
   res.json({ film });
 });
 
-router.put("/:id", function (req, res) {
+router.put("/:id", (req, res) => {
   check(res, req);
 
   films = films.map((user) => {
@@ -57,9 +69,14 @@ router.put("/:id", function (req, res) {
 router.patch("/:id", (req, res) => {
   check(res, req);
 
-  const [key] = Object.keys(req.body);
+  const keys = Object.keys(req.body);
   let foundIndex = films.findIndex((film) => film.id === +req.params.id);
-  films[foundIndex][key] = req.body[key];
+  keys.forEach((key) => {
+    films[foundIndex][key] = req.body[key];
+    console.log("76...", films[foundIndex]);
+    console.log("77...", films[foundIndex][key]);
+  });
+
   res.json({ ...films[foundIndex] });
 });
 
